783c05d919b0e35367d86afc57501f69157e70a4
Throngjwk <105552423+Throngjwk@users.noreply.github.com>
2022-07-11 09:23:41 -0300

    go

--- js/main.js
+++ js/main.js
@@ -1,7 +1,14 @@
+function GetPostion() {
+    this.x = 10;
+    this.y = 10;
+}
+
+ctxs = new GetPostion();
+
 window.onload = function() {
     var canvas = document.getElementById("myCanvas");
     var ctx = canvas.getContext("2d");
     var img = document.getElementById("apple");
-    ctx.drawImage(img, 10, 10);
+    ctx.drawImage(img, ctxs.x, ctxs.y);
   };
   
